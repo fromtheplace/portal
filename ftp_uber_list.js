14469747e707f3ba7be7802098660625cc0f8982
fromtheplace <blamboxcity@gmail.com>
2026-09-25 13:32:27 +1200

    Update ftp_uber_list.js

--- ftp_uber_list.js
+++ ftp_uber_list.js
@@ -2,11 +2,11 @@
 // Auto-updated by PLister v2
 // 2026-03-23 16:10:12
 const TRACKS = [
- { id: 'bEzAZik3WLs', "start": 532, "end": 840, title: 'Catherine KS', artist: 'Deep Love on the Horizon', genre: 'FOLK' },
- { id: 'bEzAZik3WLs', "start": 848, "end": 1080, title: 'Catherine KS', artist: 'Betrayal', genre: 'FOLK' },
- { id: 'bEzAZik3WLs', "start": 1087, "end": 1385, title: 'Catherine KS', artist: 'I Broke My Spacesuit', genre: 'FOLK' },
- { id: 'bEzAZik3WLs', "start": 1433, "end": 1622, title: 'Catherine KS', artist: 'The Center of the Sun', genre: 'FOLK' },
- { id: 'bEzAZik3WLs', "start": 1643, "end": 1980, title: 'Catherine KS', artist: 'The Velveteen Rabbit', genre: 'FOLK' },
+  { id: 'bEzAZik3WLs', "start": 532, "end": 840, artist: 'Catherine KS', title: 'Deep Love on the Horizon', genre: 'FOLK' },
+ { id: 'bEzAZik3WLs', "start": 848, "end": 1080, artist: 'Catherine KS', title: 'Betrayal', genre: 'FOLK' },
+ { id: 'bEzAZik3WLs', "start": 1087, "end": 1385, artist: 'Catherine KS', title: 'I Broke My Spacesuit', genre: 'FOLK' },
+ { id: 'bEzAZik3WLs', "start": 1433, "end": 1622, artist: 'Catherine KS', title: 'The Center of the Sun', genre: 'FOLK' },
+ { id: 'bEzAZik3WLs', "start": 1643, "end": 1980, artist: 'Catherine KS', title: 'The Velveteen Rabbit', genre: 'FOLK' },
   { id: 'bEzAZik3WLs', "start": 2477, "end": 4752, title: 'Person Will', artist: 'Lobofest', genre: 'ALT' },
   { id: 'bEzAZik3WLs', "start": 5268, "end": 7144, title: 'Baser & Allen', artist: 'Lobofest', genre: 'folk' },
   { id: 'bEzAZik3WLs', "start": 7791, "end": 8657, title: 'Alan Ned Gray', artist: 'Lobofest', genre: 'folk' },
